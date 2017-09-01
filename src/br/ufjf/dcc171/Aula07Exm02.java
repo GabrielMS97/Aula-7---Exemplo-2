cb5e47185bf01f2b0d8925c27e59b19dae6aa323
ice <ice@localhost>
2017-08-31 21:51:54 -0300

    Aula 7 - Exemplo 2

---
+++ src/br/ufjf/dcc171/Aula07Exm02.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JFrame;
+
+public class Aula07Exm02 {
+
+    public static void main(String[] args) {
+        JanelaListaPessoa j = new JanelaListaPessoa();
+        j.setSize(400, 200);
+        j.setVisible(true);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setLocationRelativeTo(null);
+    }
+
+}
